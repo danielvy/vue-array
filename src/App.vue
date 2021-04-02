10d24f38956ca40a7f14e8c861fd38b2a4f82e74
danielv <daniel@eliyot.com>
2021-04-02 19:32:10 +0300

    added problem reproduction

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+	<h1>Vue array type</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+interface MyArray extends Array<number> {};
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
+	data() {
+		return {
+			array: [] as MyArray
+		}
+	},
+
+	mounted() {
+		const x: Array<number> = this.array;
+		const y: MyArray = this.array;
+		console.log(x, y);
+	}
+
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
